24e35bcc6f375ec69e032862d958cd0066981c75
Joseph Moskie <jmoskie@cisco.com>
2016-08-06 14:48:31 -0700

    Added all game-icons, hexes are SVG groups now

--- js/hex.js
+++ js/hex.js
@@ -5,42 +5,101 @@ HEX_TYPES =
 	[
 		{
 			"name" 		: "water",
-			"color" 	: "#83C6F2",
+			"color" 	: "#1A6ABA",
+			"image" 	: "at-sea",
 			"isPassable": false,
 			"moveCost" 	: 0
 		},
 		{
 			"name" 		: "grassland",
 			"color" 	: "#7DBF50",
+			"image" 	: "high-grass",
 			"isPassable": true,
 			"moveCost" 	: 1
 		},
 		{
 			"name" 		: "hill",
 			"color" 	: "#B0A03C",
+			"image" 	: "hills",
 			"isPassable": true,
 			"moveCost" 	: 2
 		},
 		{
 			"name" 		: "mountain",
 			"color" 	: "#634B15",
+			"image" 	: "peaks",
 			"isPassable": false,
+			"moveCost" 	: 0
+		},
+		{
+			"name" 		: "forest",
+			"color" 	: "#117D44",
+			"image"		: "forest",
+			"isPassable": true,
 			"moveCost" 	: 0
 		}
 	];
 
 
 
-function Hex (row, col, type, polygon, svgCanvas) {
+function Hex (row, col, type, svgCanvas) {
 	this.row = row;
 	this.col = col;
 	this.type = type;
-    this.polygon = polygon.fill(HEX_TYPES[type].color);
-    this.svgCanvas = svgCanvas;
+	this.svgCanvas = svgCanvas;
+
+	dx = 56.25 * col;
+	dy = 65 * row + (col % 2 ? 32.5 : 0);
+
+	points = [[75+dx,32.5+dy], [56.25+dx,65+dy], [18.75+dx,65+dy], [0+dx,32.5+dy], [18.75+dx,0+dy], [56.25+dx,0+dy]];
+
+	this.group = this.svgCanvas.group();
+
+	polygon = this.svgCanvas.polygon().addClass("hex").fill("#E6E6E6").stroke({color: '#303030', width: 0.5}).plot(points).fill(HEX_TYPES[type].color);;
+	this.group.add(polygon);
+
+	// add background image, if this hex type has one
+	this.setBackgroundImage(HEX_TYPES[type].image, 0.30);
+
+	// TODO: Move this out of the loop...
+	this.group.on('click', this.hexGroupClick);
+
+	this.group.mouseover(function() {
+		this.get(0).addClass("hoveredHex");
+		this.front();
+	});
+
+	this.group.mouseout(function() {
+		this.get(0).removeClass("hoveredHex");
+		this.front();
+	});
 }
 
-Hex.prototype.addImage = function() {
-	this.image = this.svgCanvas.image('images/game-icons.net/robe.svg')
-					 .size(ICON_SIZE, ICON_SIZE)
-					 .center(this.polygon.cx(), this.polygon.cy());
+Hex.prototype.getPolygon = function() {
+	return this.group.get(0);
+}
+
+Hex.prototype.getBackgroundImage = function() {
+	return this.group.get(1);
+}
+
+Hex.prototype.setBackgroundImage = function(image, opacity) {
+	if(image) {
+		image = this.svgCanvas.image('images/game-icons.net/' + image + '.svg')
+						 .size(ICON_SIZE, ICON_SIZE)
+						 .center(this.getPolygon().cx(), this.getPolygon().cy())
+						 .opacity(opacity);
+
+		this.group.add(image);
+	}
+}
+
+Hex.prototype.hexGroupClick = function(event) {
+	this.get(0).toggleClass("selectedHex");
+
+	if(this.get(0).hasClass("selectedHex")) {
+		// selected
+	} else {
+		// deselected
+	}
 }
--- js/map.js
+++ js/map.js
@@ -5,44 +5,19 @@ function Map (canvasId, height, width) {
     // TODO: all of these values are coded-coded for expediency. more effort should be put into this later.
     this.svgCanvas = SVG(canvasId).size(56.25 * width + 25, 65 * height + 50);
 
-    this.generateMap(height, width);
+    this.generateTestMap(height, width);
     // this.preloadImages();
 }
 
-Map.prototype.generateMap = function(height, width) {
+Map.prototype.generateTestMap = function(height, width) {
 	for(row=0; row<height; row++) {
 		this.map[row] = Array();
 
 		for(col=0; col<width; col++) {
-			dx = 56.25 * col;
-			dy = 65 * row + (col % 2 ? 32.5 : 0);
-
-			points = [[75+dx,32.5+dy], [56.25+dx,65+dy], [18.75+dx,65+dy], [0+dx,32.5+dy], [18.75+dx,0+dy], [56.25+dx,0+dy]];
-			polygon = this.svgCanvas.polygon().addClass("hex").fill("#E6E6E6").stroke({color: '#303030', width: 0.5}).plot(points);
-
-			// TODO: Move this out of the loop...
-			polygon.click(function() {
-				this.toggleClass("selectedHex");	// .front() blocks icons. need a group!
-
-				if(this.hasClass("selectedHex")) {
-					// selected
-				} else {
-					// deselected
-				}
-			});
-
-			polygon.mouseover(function() {
-				this.addClass("hoveredHex").front();
-			});
-
-			polygon.mouseout(function() {
-				this.removeClass("hoveredHex").front();
-			});
-
 			// give the hex a random type, based on the values defined in HEX_TYPES
 			var type = Math.floor(Math.random() * HEX_TYPES.length);
 
-			this.map[row][col] = new Hex(row, col, type, polygon, this.svgCanvas);
+			this.map[row][col] = new Hex(row, col, type, this.svgCanvas);
 		}
 	}
 }
