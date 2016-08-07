af977c22b48ed8ba7abca44469fc067439a7b9fa
Joseph Moskie <jmoskie@cisco.com>
2016-08-06 23:48:47 -0700

    Symmetric map generator. Dynamic Groups. Units.

--- js/hex.js
+++ js/hex.js
@@ -1,99 +1,144 @@
-ICON_SIZE = 50;
+BACKGROUND_ICON_SIZE = 50;
+UNIT_ICON_SIZE = 40;
 
 // TODO: it would be smart to move this to json files for different games, or something.
-HEX_TYPES =
-	[
-		{
-			"name" 		: "water",
-			"color" 	: "#1A6ABA",
-			"image" 	: "at-sea",
-			"isPassable": false,
-			"moveCost" 	: 0
-		},
-		{
-			"name" 		: "grassland",
-			"color" 	: "#7DBF50",
-			"image" 	: "high-grass",
-			"isPassable": true,
-			"moveCost" 	: 1
-		},
-		{
-			"name" 		: "hill",
-			"color" 	: "#B0A03C",
-			"image" 	: "hills",
-			"isPassable": true,
-			"moveCost" 	: 2
-		},
-		{
-			"name" 		: "mountain",
-			"color" 	: "#634B15",
-			"image" 	: "peaks",
-			"isPassable": false,
-			"moveCost" 	: 0
-		},
-		{
-			"name" 		: "forest",
-			"color" 	: "#117D44",
-			"image"		: "forest",
-			"isPassable": true,
-			"moveCost" 	: 0
-		}
-	];
+HEX_TYPES =[
+	{
+		"name" 		: "water",
+		"color" 	: "#1A6ABA",
+		"image" 	: "at-sea",
+		"isPassable": false,
+		"moveCost" 	: 0
+	},
+	{
+		"name" 		: "grassland",
+		"color" 	: "#7DBF50",
+		"image" 	: "high-grass",
+		"isPassable": true,
+		"moveCost" 	: 1
+	},
+	{
+		"name" 		: "hill",
+		"color" 	: "#B0A03C",
+		// "image" 	: "hills",
+		"isPassable": true,
+		"moveCost" 	: 2
+	},
+	{
+		"name" 		: "mountain",
+		"color" 	: "#634B15",
+		"image" 	: "peaks",
+		"isPassable": false,
+		"moveCost" 	: 0
+	},
+	{
+		"name" 		: "forest",
+		"color" 	: "#117D44",
+		"image"		: "forest",
+		"isPassable": true,
+		"moveCost" 	: 0
+	}
+];
+
+HEX_GROUPS = {"background":0, "structure":1, "unit":2};
 
 
-
-function Hex (row, col, type, svgCanvas) {
+function Hex (row, col, type, mapGroup) {
 	this.row = row;
 	this.col = col;
-	this.type = type;
-	this.svgCanvas = svgCanvas;
+	this.mapGroup = mapGroup;
+	this.hexGroup = mapGroup.group();
 
 	dx = 56.25 * col;
 	dy = 65 * row + (col % 2 ? 32.5 : 0);
 
 	points = [[75+dx,32.5+dy], [56.25+dx,65+dy], [18.75+dx,65+dy], [0+dx,32.5+dy], [18.75+dx,0+dy], [56.25+dx,0+dy]];
 
-	this.group = this.svgCanvas.group();
+	groups = Array();
+	groupCount = Object.keys(HEX_GROUPS).length;
 
-	polygon = this.svgCanvas.polygon().addClass("hex").fill("#E6E6E6").stroke({color: '#303030', width: 0.5}).plot(points).fill(HEX_TYPES[type].color);;
-	this.group.add(polygon);
+	for(i=0; i<groupCount; i++)
+		groups.push(this.hexGroup.group());
+
+	// we're going to add some stuff to the background group to represent the base hex
+	backgroundGroup = groups[0];
+	structureGroup = groups[1];
+	unitGroup = groups[2];
+
+	polygon = backgroundGroup.polygon().addClass("hex").fill("#E6E6E6").stroke({color: '#303030', width: 0.5}).plot(points);
 
 	// add background image, if this hex type has one
-	this.setBackgroundImage(HEX_TYPES[type].image, 0.30);
+	this.setType(type);
 
 	// TODO: Move this out of the loop...
-	this.group.on('click', this.hexGroupClick);
+	this.hexGroup.on('click',		this.hexGroupClick);
+	this.hexGroup.on('mousedown',	this.mousedown);
+	this.hexGroup.on('mouseup',	this.mouseup);
 
-	this.group.mouseover(function() {
-		this.get(0).addClass("hoveredHex");
-		this.front();
+	this.hexGroup.mouseover(function() {
+		this.get(HEX_GROUPS["background"]).get(0).addClass("hoveredHex");
 	});
 
-	this.group.mouseout(function() {
-		this.get(0).removeClass("hoveredHex");
-		this.front();
+	this.hexGroup.mouseout(function() {
+		this.get(HEX_GROUPS["background"]).get(0).removeClass("hoveredHex");
 	});
 }
 
+Hex.prototype.setType = function(type) {
+	this.type = type;
+
+	this.getPolygon().fill(HEX_TYPES[type].color);
+	this.setBackgroundImage(HEX_TYPES[type].image, 0.30);
+}
+
+// background tile groups get sepcial helper functions for the their polygon and icon
 Hex.prototype.getPolygon = function() {
-	return this.group.get(0);
+	return this.hexGroup.get(HEX_GROUPS["background"]).get(0);
 }
 
 Hex.prototype.getBackgroundImage = function() {
-	return this.group.get(1);
+	return this.hexGroup.get(HEX_GROUPS["background"]).get(1);
 }
 
-Hex.prototype.setBackgroundImage = function(image, opacity) {
-	if(image) {
-		image = this.svgCanvas.image('images/game-icons.net/' + image + '.svg')
-						 .size(ICON_SIZE, ICON_SIZE)
+Hex.prototype.setBackgroundImage = function(image="blank", opacity) {
+	image = this.hexGroup.image('images/game-icons.net/' + image + '.svg')
+						 .size(BACKGROUND_ICON_SIZE, BACKGROUND_ICON_SIZE)
 						 .center(this.getPolygon().cx(), this.getPolygon().cy())
 						 .opacity(opacity);
 
-		this.group.add(image);
+	if(!this.getBackgroundImage()) {
+		this.hexGroup.get(HEX_GROUPS["background"]).add(image);
+	} else {
+		this.getBackgroundImage().replace(image);
 	}
 }
 
+// TODO: this is getting too specific already. If this is supposed to be a general purpose library, I don't think I should be drawing units like this.
+Hex.prototype.setUnitImage = function(image="blank", opacity) {
+	unitGroup = this.hexGroup.get(HEX_GROUPS["unit"]).draggable();
+	// if this is the first unit, draw the background disc
+	if(!unitGroup.get(0)) {
+		unitGroup.circle(40)
+			     .fill("#FFFFFF")
+			     .stroke({color: '#000000', width: 1.0})
+			     .center(this.getPolygon().cx(), this.getPolygon().cy());
+
+	}
+
+	unitGroup.image('images/game-icons.net/' + image + '.svg')
+			 .size(UNIT_ICON_SIZE, UNIT_ICON_SIZE)
+			 .center(this.getPolygon().cx(), this.getPolygon().cy())
+			 .opacity(opacity)
+}
+
+
+// other tile groups are more generic, and can contain multiple child items
+Hex.prototype.getGroup = function(groupName) {
+	return this.hexGroup.get(HEX_GROUPS[groupName]);
+}
+
+
+// events
 Hex.prototype.hexGroupClick = function(event) {
 	this.get(0).toggleClass("selectedHex");
 
@@ -102,4 +147,12 @@ Hex.prototype.hexGroupClick = function(event) {
 	} else {
 		// deselected
 	}
+}
+
+Hex.prototype.mouseup = function(event) {
+	// console.log(event);
+}
+
+Hex.prototype.mousedown = function(event) {
+	// console.log(event);
 }
--- js/map.js
+++ js/map.js
@@ -1,28 +1,78 @@
-function Map (canvasId, height, width) {
+// TODO: all of these values are coded-coded for expediency. more effort should be put into this later.
+function Map (canvasId, rows, cols, type, height=75 * rows, width=59 * cols + 25) {
     this.map = new Array(new Array());
     this.selectedHexes = new Array();
 
-    // TODO: all of these values are coded-coded for expediency. more effort should be put into this later.
-    this.svgCanvas = SVG(canvasId).size(56.25 * width + 25, 65 * height + 50);
+    boxHeight = 75 * rows;
+    boxWidth  = 59 * cols + 25;
 
-    this.generateTestMap(height, width);
-    // this.preloadImages();
+    this.svgCanvas = SVG(canvasId).size(width, height);
+
+    dragConstraints = {minX: -boxWidth/2, minY: -boxHeight/2, maxX: boxWidth, maxY: boxHeight};
+
+    this.mapGroup = this.svgCanvas.group().draggable(dragConstraints).on('dragmove', this.dragmove).on('dragend', this.dragend);
+
+    switch(type) {
+		case "symmetric":
+			this.generateSymmetricMap(rows, cols);
+			break;
+		case "random":
+			default:
+			this.generateTestMap(rows, cols);
+			break;
+    }
+
+    // center the map
+    this.mapGroup.center(this.svgCanvas.cx(), this.svgCanvas.cy());
+
+    // add some units!
+    rndRow = Math.floor(Math.random() * rows);
+    rndCol = Math.floor(Math.random() * cols);
+	this.map[rndRow][rndCol].setUnitImage("barbarian", 0.8);
+
+	rndRow = Math.floor(Math.random() * rows);
+    rndCol = Math.floor(Math.random() * cols);
+	this.map[rndRow][rndCol].setUnitImage("orc-head", 0.8);
 }
 
-Map.prototype.generateTestMap = function(height, width) {
-	for(row=0; row<height; row++) {
+Map.prototype.dragmove = function(event) {
+	// console.log(event);
+}
+
+Map.prototype.dragend = function(event) {
+	// console.log("=======END=======");
+	// console.log(event);
+}
+
+Map.prototype.generateTestMap = function(rows, cols) {
+	for(row=0; row<rows; row++) {
 		this.map[row] = Array();
 
-		for(col=0; col<width; col++) {
+		for(col=0; col<cols; col++) {
 			// give the hex a random type, based on the values defined in HEX_TYPES
 			var type = Math.floor(Math.random() * HEX_TYPES.length);
 
-			this.map[row][col] = new Hex(row, col, type, this.svgCanvas);
+			this.map[row][col] = new Hex(row, col, type, this.mapGroup);
 		}
 	}
 }
 
-Map.prototype.preloadImages = function() {
-	this.svgCanvas.image('images/game-icons.net/blood.svg').hide();
-	this.svgCanvas.image('images/game-icons.net/robe.svg').hide();
+// this needs to be called with an even-numbered width
+Map.prototype.generateSymmetricMap = function(rows, cols) {
+	for(row=0; row<rows; row++) {
+		this.map[row] = Array();
+		for(col=0; col<cols/2; col++) {
+			// console.log(row + "," + col + "height: " + height + ", height-row=" + (height - row));
+
+			// give the hex a random type, based on the values defined in HEX_TYPES
+			var type = Math.floor(Math.random() * HEX_TYPES.length);
+
+			this.map[row][col] = new Hex(row, col, type, this.mapGroup);
+			this.map[row][cols - col] = new Hex(row, cols - col, type, this.mapGroup);
+		}
+
+		// the middle, joining column
+		var type = Math.floor(Math.random() * HEX_TYPES.length);
+		this.map[row][col] = new Hex(row, col, type, this.mapGroup);
+	}
 }
